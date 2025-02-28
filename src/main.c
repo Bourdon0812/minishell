d07976d989f88bb608359c34f84f0cadfe7b6ddf
yseguin <youvataque@icloud.com>
2025-02-28 15:16:26 +0100

    version à priori parfaite
    Il faut commenter les "rl_replace_line" sur mac os car il utilise libedit au lieu de readline par défaut.

--- src/main.c
+++ src/main.c
@@ -6,52 +6,100 @@
 /*   By: yseguin <yseguin@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 12:36:26 by yseguin           #+#    #+#             */
-/*   Updated: 2025/02/27 12:50:23 by yseguin          ###   ########.fr       */
+/*   Updated: 2025/02/28 15:15:28 by yseguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+///////////////////////////////////////////////////////////////////////////////
+// rewrite shell for ctrl c
 void	handle_sigint(int sig)
 {
 	(void)sig;
-	write(STDOUT_FILENO, GREEN "\nminishell> " RESET, 23);
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// check if the -c is here
 int	check_opt(int ac, char **av)
 {
 	if (ac == 3)
 	{
-		if (ft_strncmp("-c", av[2], ft_strlen(av[2])))
+		if (ft_strncmp("-c", av[1], 2) == 0)
 			return (1);
 	}
 	return (0);
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// control args (good, wrong, with -c without etc).
+int	check_args(int ac, char **av, char **input)
+{
+	if (ac == 2 || ac > 3)
+		return (0);
+	if (ac == 3 && !check_opt(ac, av))
+		return (0);
+	else
+	{
+		if (!check_opt(ac, av))
+			*input = readline(GREEN "minishell> " RESET);
+		else
+		{
+			*input = av[2];
+			return (2);
+		}
+		return (1);
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// function for the input actions (clean, addHistory, readline, etc)
+int	input_act(char **input)
+{
+	if (*input == NULL)
+		return (0);
+	if (ft_strncmp(*input, "exit", 5) == 0)
+	{
+		free(*input);
+		return (0);
+	}
+	if (**input != '\0')
+	{
+		add_history(*input);
+		write_history(".minishell_history");
+	}
+	free(*input);
+	rl_replace_line("", 0);
+	*input = readline(GREEN "minishell> " RESET);
+	if (*input == NULL)
+		return (0);
+	return (1);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Main function, start the soft.
 int	main(int ac, char **av)
 {
 	char	*input;
+	int		check;
 
 	signal(SIGINT, handle_sigint);
 	while (1)
 	{
-		if (!check_opt(ac, av))
-			input = readline(GREEN "minishell> " RESET);
+		check = check_args(ac, av, &input);
+		if (check == 2)
+			return (0);
+		else if (check == 0)
+			return (ft_printf("Error wrong args\n"), 1);
 		else
-			input = av[3];
-		if (input == NULL)
 		{
-			printf("exit\n");
-			break ;
+			if (!input_act(&input))
+				break ;
 		}
-		if (ft_strncmp(input, "exit", 6) == 0)
-		{
-			free(input);
-			break ;
-		}
-		add_history(input);
-		free(input);
 	}
 	return (0);
 }
