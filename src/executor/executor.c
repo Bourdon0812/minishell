ccf8cf750321774945e96308ec76f4b848390556
yseguin <youvataque@icloud.com>
2025-03-09 12:39:16 +0100

    Correction de divers bug

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,30 +6,39 @@
 /*   By: yseguin <yseguin@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 01:37:44 by ilbonnev          #+#    #+#             */
-/*   Updated: 2025/03/05 15:42:02 by yseguin          ###   ########.fr       */
+/*   Updated: 2025/03/09 12:36:27 by yseguin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+///////////////////////////////////////////////////////////////////////////////
+// Function for complexe command
 void	complex_command(t_shell *shell)
 {
 	return ;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// Function for the single command without >>, <, | 
 void	simple_command(t_shell *shell)
 {
-	int	i;
+	int	status;
+	int	pid;
 
-	i = 0;
-	while (shell->cmd[i] != NULL)
-	{
-		ft_putnbr_fd(ft_strlen(shell->cmd[i]), 1);
-		ft_putchar_fd('\n', 1);
-		i++;
-	}
 	if (is_builtins(shell->cmd[0]))
 		exe_builtins(shell);
 	else
-		binaries_in_out(shell, STDIN_FILENO, STDOUT_FILENO);
+	{
+		pid = fork();
+		if (pid == -1)
+			ft_printf("Error\n");
+		if (pid == 0)
+		{
+			binaries_in_out(shell, STDIN_FILENO, STDOUT_FILENO);
+			exit(1);
+		}
+		else
+			waitpid(pid, &status, 0);
+	}
 }
