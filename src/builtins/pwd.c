ffea1ca48f9f5637cb8c8118bcb1e7225b1f4383
Bourdon <69124289+Bourdon0812@users.noreply.github.com>
2025-03-31 18:07:49 +0200

    add pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ilbonnev <ilbonnev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 15:51:57 by ilbonnev          #+#    #+#             */
-/*   Updated: 2025/03/20 15:45:36 by ilbonnev         ###   ########.fr       */
+/*   Updated: 2025/03/31 16:48:06 by ilbonnev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	exe_pwd(t_shell *shell, char **args)
 {
-	return ;
+	char	cwd[4096];
+
+	(void)shell;
+	(void)args;
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+	{
+		perror("pwd");
+		return ;
+	}
+	ft_printf("%s\n", cwd);
 }
