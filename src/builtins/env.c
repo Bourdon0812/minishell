4875c03ebfe20c1bd0ba4b828747269dfe999e88
Bourdon <69124289+Bourdon0812@users.noreply.github.com>
2025-03-31 16:40:32 +0200

    add env

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: ilbonnev <ilbonnev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 15:51:46 by ilbonnev          #+#    #+#             */
-/*   Updated: 2025/03/20 15:45:21 by ilbonnev         ###   ########.fr       */
+/*   Updated: 2025/03/31 16:39:06 by ilbonnev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void	exe_env(t_shell *shell, char **args)
 {
-	return ;
+	int	i;
+
+	(void)args;
+	i = -1;
+	if (!(shell->envp))
+		return ;
+	while (shell->envp[++i])
+		if (ft_strchr(shell->envp[i], '='))
+			ft_printf("%s\n", shell->envp[i]);
 }
